b43b7f07a71867ea92d5e7ff6e6b69fd5ab944dd
afify <hassan@afify.dev>
2021-06-01 00:10:30 +0300

    add connec_irc.sh

---
+++ .scripts/connec_irc.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+/server add libera irc.libera.chat/6667 -autoconnect
+/set irc.server.libera.nicks afify
+/connect libera
+
+openssl ecparam -genkey -name prime256v1 -out ~/.weechat/ecdsa.pem
+openssl ec -noout -text -conv_form compressed -in ~/.weechat/ecdsa.pem | grep '^pub:' -A 3 | tail -n 3 | tr -d ' \n:' | xxd -r -p | base64
+
+/msg nickserv set pubkey A/80odYfSlgrFCpaW8Og9Z4HfxGQUTGGto+f/kZ6+Pjc
+/set irc.server.libera.sasl_mechanism ecdsa-nist256p-challenge
+/set irc.server.libera.sasl_username "afify"
+/set irc.server.libera.sasl_key "%h/ecdsa.pem"
+
+/reconnect libera
+
+/set irc.server.libera.addresses "libera75jm6of4wxpxt4aynol3xjmbtxgfyjpu34ss4d7r7q2v5zrpyd.onion"
+/proxy add tor socks5 127.0.0.1 9050
+/set irc.server.libera.proxy "tor"
+
+/set irc.server.libera.ssl_verify off
+/reconnect libera
+
+/set username "afify"
+/set irc.server_default.msg_part ""
+/set irc.server_default.msg_quit ""
+/set irc.ctcp.clientinfo ""
+/set irc.ctcp.finger ""
+/set irc.ctcp.source ""
+/set irc.ctcp.time ""
+/set irc.ctcp.userinfo ""
+/set irc.ctcp.version ""
+/set irc.ctcp.ping ""
+/plugin unload xfer
+/set weechat.plugin.autoload "*,!xfer"
+/save
+/reconnect libera
+
