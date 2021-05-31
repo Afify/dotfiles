4edbdb9ed1666377809a5319b254fe39f49117ec
afify <hassan@afify.dev>
2021-06-01 02:59:55 +0300

    libera add cloak

--- .scripts/connec_irc.sh
+++ .scripts/connec_irc.sh
@@ -7,21 +7,21 @@
 openssl ecparam -genkey -name prime256v1 -out ~/.weechat/ecdsa.pem
 openssl ec -noout -text -conv_form compressed -in ~/.weechat/ecdsa.pem | grep '^pub:' -A 3 | tail -n 3 | tr -d ' \n:' | xxd -r -p | base64
 
-/msg nickserv set pubkey A/80odYfSlgrFCpaW8Og9Z4HfxGQUTGGto+f/kZ6+Pjc
+/msg NickServ IDENTIFY
+/msg nickserv set pubkey AqiokYOwutN60SSviH1itTwPeNnqcCLY+wHsNg4HEu7V
 /set irc.server.libera.sasl_mechanism ecdsa-nist256p-challenge
 /set irc.server.libera.sasl_username "afify"
 /set irc.server.libera.sasl_key "%h/ecdsa.pem"
-
 /reconnect libera
 
 /set irc.server.libera.addresses "libera75jm6of4wxpxt4aynol3xjmbtxgfyjpu34ss4d7r7q2v5zrpyd.onion"
 /proxy add tor socks5 127.0.0.1 9050
 /set irc.server.libera.proxy "tor"
-
 /set irc.server.libera.ssl_verify off
 /reconnect libera
 
-/set username "afify"
+/set irc.server_default.username "afify"
+/set irc.server_default.realname "afify"
 /set irc.server_default.msg_part ""
 /set irc.server_default.msg_quit ""
 /set irc.ctcp.clientinfo ""
@@ -36,3 +36,10 @@ openssl ec -noout -text -conv_form compressed -in ~/.weechat/ecdsa.pem | grep '^
 /save
 /reconnect libera
 
+# became operator
+/msg chanserv op #sfm afify
+/msg Chanserv set #sfm guard on
+/mode #sfm
+/msg chanserv info #sfm
+/join #libera-cloak
+/msg #libera-cloak !cloakme
