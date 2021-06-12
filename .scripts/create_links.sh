5eafb6bae03b4a28c7e34f8e6721a061e457997e
afify <hassan@afify.dev>
2021-06-12 11:06:11 +0300

    [feat] create_links

--- .scripts/create_links.sh
+++ .scripts/create_links.sh
@@ -2,19 +2,22 @@
 
 [ "$USER" != "root" ] && echo "Must run as root." && exit 1
 
-ln -s /home/hassan/.vimrc	/root/.vimrc
-ln -s /home/hassan/.vim		/root/.vim
-ln -s /mnt/data/todo.txt	/home/hassan/
-#ln -s /usr/bin/nvim		/bin/vim
+mkdir /root/.config
+ln -s /home/hassan/.local/share/nvim/site /root/.local/share/nvim/site
+ln -s /home/hassan/.config/nvim   /root/.config/nvim
+ln -s /home/hassan/.vim           /root/.vim
+ln -s /mnt/data/todo.txt          /home/hassan/
 
 if [ "$(uname)" = "Linux" ]; then
 	ln -s /home/hassan/android-studio/bin/studio.sh /usr/local/bin/android-studio
-	ln -fs /bin/clang		/bin/cc
-	ln -fs /bin/ld.lld		/bin/ld
-	ln -s /etc/sv/wpa_supplicant	/var/service/
-	ln -s /etc/sv/ntpd		/var/service/
-	ln -s /etc/sv/sshd		/var/service/
-	ln -s /etc/sv/bluetoothd	/var/service/
+	ln -fs /bin/clang               /bin/cc
+	ln -fs /bin/ld.lld              /bin/ld
+	ln -s /etc/sv/wpa_supplicant    /var/service/
+	ln -s /etc/sv/ntpd              /var/service/
+	ln -s /etc/sv/apache            /var/service/
+	ln -sf /etc/sv/mysqld           /var/service/
+# 	ln -s /etc/sv/sshd		/var/service/
+# 	ln -s /etc/sv/bluetoothd	/var/service/
 fi
 
 [ "$(uname)" = "OpenBSD" ] && ln -s /home/hassan/dev /mnt/data/dev
